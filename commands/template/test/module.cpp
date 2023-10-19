f2a0811093152ffdbce3b8c809630594d2511f15
im-zhong <84006787+im-zhong@users.noreply.github.com>
2023-10-19 21:28:41 +0800

    Update module.cpp

--- commands/template/test/module.cpp
+++ commands/template/test/module.cpp
@@ -2,7 +2,7 @@
 // zhangzhong
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest/doctest.h"
+#include <doctest/doctest.h>
 #include <iostream>
 
 TEST_CASE("testing module") { std::cout << "hello module" << std::endl; }
