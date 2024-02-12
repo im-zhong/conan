b1b21b630de485a4538f51396c9fb3f274f6eaac
zhangzhong <im.zhong@outlook.com>
2024-02-12 17:19:17 +0800

    format files

--- install.py
+++ install.py
@@ -4,13 +4,10 @@
 import shutil
 import os
 
-home = os.path.expanduser('~')
-conan_home = f'{home}/.conan2'
-commands_path = f'{conan_home}/extensions/commands'
-templates_path = f'{conan_home}/templates/command/new'
+home = os.path.expanduser("~")
+conan_home = f"{home}/.conan2"
+commands_path = f"{conan_home}/extensions/commands"
+templates_path = f"{conan_home}/templates/command/new"
 
-shutil.copytree(
-    './commands', commands_path, dirs_exist_ok=True)
-shutil.copytree(
-    './templates', templates_path, dirs_exist_ok=True
-)
+shutil.copytree("./commands", commands_path, dirs_exist_ok=True)
+shutil.copytree("./templates", templates_path, dirs_exist_ok=True)
